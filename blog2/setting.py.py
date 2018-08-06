d5aff8d58636b830d10e8f17a7f27b8dbd232d08
chaodeng <645111208@qq.com>
2018-08-06 16:05:06 +0800

    delete setting

--- blog2/setting.py.py
+++
@@ -1,6 +0,0 @@
-# _*_ coding: utf-8 _*_
-
-DEBUG = True
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-SECRET_KEY='A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
-SQLALCHEMY_DATABASE_URI = "mysql://chao:chao1006@127.0.0.1:3306/db"
